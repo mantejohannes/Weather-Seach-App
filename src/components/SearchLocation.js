6b8d828d98e4db768e308b3c31c53ba109a83bb2
mantejohannes <mokibelomante@gmail.com>
2023-07-21 11:34:05 +0200

    first commit

--- src/components/SearchLocation.js
+++ src/components/SearchLocation.js
@@ -6,20 +6,30 @@ import axios from "axios";
 function SearchLocation(){
     const [data, setData] = useState({})
     const [place, setPlace] = useState('')
+    const [error, setError] = useState(null); // State to handle errors
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${place}&units=imperial&appid=8cce937b472bcf916d60bbbec1d2d248
-    `
+
+    const apiKey = "8cce937b472bcf916d60bbbec1d2d248";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${place}&units=imperial&appid=${apiKey}`
     const SearchLocation = (event) => {
         if (event.key === 'Enter'){
             axios.get(url).then((response) =>{
-                setData(response.data)
-                console.log(response.data)
-            })
+                setData(response.data);
+                setError(null); 
+
+             }).catch((error) => {
+                setError("Error fetching weather data. Please check the location or try again later.");
+                console.error(error);
+              });
             setPlace('')
         }
        
     }
    
+
+    const getWeatherIconUrl = (weatherIconCode) => {
+        return `http://openweathermap.org/img/wn/${weatherIconCode}.png`;
+      }
 
 
 
@@ -33,32 +43,54 @@ function SearchLocation(){
                 placeholder="Enter your Location"
                 type="text" />
             </div>
+
+
             <div className="container">
                 <div className="header">
-                    <div className="location">
-                        <p>{data.name}</p>
-                    </div>
+                    
 
                     <div className="temperature">
                         {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null }
-                       
+                        <div className="locations">
+                        <p>{data.name}</p>
                     </div>
+                        <div className="description">
 
-                    <div className="description">
                     {data.weather ? <p>{data.weather[0].main}</p> : null }
-
                     </div>
-                </div>
 
-            {data.name != undefined&&
+                    <div className="icon">
 
-                <div className="footer">
+                    {data.weather ? <img src={getWeatherIconUrl(data.weather[0].icon)} alt={data.weather[0].main} /> : null}
+
+                  </div>
+
+                    
+
+                    
+                    <br></br>
+
+                    {data.name !== undefined&&
+
                     <div className="feels">
+                        <p>Feels like</p>
                     {data.main ? <p className="bold">{data.main.feels_like.toFixed()}°F</p> : null }
 
-                        <p>Feels like</p>
+                        
 
                     </div>
+
+                    }
+                      
+                    </div>
+
+                    
+                </div>
+
+            {data.name !== undefined&&
+
+                <div className="footer">
+                    
 
                     <div className="humidity">
                     {data.main ? <p className="bold">{data.main.humidity}%</p> : null }
@@ -72,10 +104,13 @@ function SearchLocation(){
 
                         <p>Wind Speed</p>
                     </div>
+
+                    
                  </div>
                 }
             </div>
-                 
+            {error && <p className="error-message">{error}</p>}
+
         </div>
     )
 }
